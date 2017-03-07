b84b97c391f5c9d375d07e0b936f5a3e778f2928
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2017-03-07 07:07:43 -0600

    feat(theme): Add example on changing color theme

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
+import { AureliaUX } from 'aurelia-ux';
+import { inject } from 'aurelia-dependency-injection';
+
+@inject(AureliaUX)
 export class App {
-  constructor() {
+  constructor(ux) {
+    this.ux = ux;
+
+    ux.design.primary = '#704794';
+    ux.design.accent = '#e62787';
+
     this.message = 'Hello World!';
+  }
+
+  changeColors() {
+    this.ux.design.accent = '#00b8ff';
   }
 }
